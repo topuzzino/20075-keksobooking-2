b025a9e1f66c71a527e91d473ea8f983462ef672
Gerza <topuzzino@hotmail.de>
2018-11-21 20:31:31 +0100

    Add validation of the form and reset

--- js/map.js
+++ js/map.js
@@ -31,6 +31,14 @@ var CHECKIN_TEXT = 'Заезд после ';
 var CHECKOUT_TEXT = ', выезд до ';
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
+var MAX_ROOM_NUMBER = 100;
+var NO_GUEST_VALUE = 0;
+var minPrice = {
+  bungalo: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000
+};
 
 // Finding nessersary elements in DOM
 var mapElement = document.querySelector('.map');
@@ -43,6 +51,14 @@ var adForm = document.querySelector('.ad-form');
 var adFormFieldsets = adForm.querySelectorAll('fieldset');
 var mainPin = mapElement.querySelector('.map__pin--main');
 var addressInput = adForm.querySelector('#address');
+var priceInput = adForm.querySelector('#price');
+var typeField = adForm.querySelector('#type');
+var checkInField = adForm.querySelector('#timein');
+var checkOutField = adForm.querySelector('#timeout');
+var capacityField = adForm.querySelector('#capacity');
+var roomNumberField = adForm.querySelector('#room_number');
+var resetButton = adForm.querySelector('.ad-form__reset');
+var mapPins = [];
 
 // Function for getting a random element from an array
 var getRandomElement = function (array) {
@@ -73,7 +89,6 @@ var disableForm = function () {
     item.setAttribute('disabled', 'disabled');
   });
 };
-disableForm();
 
 // Function for showing/activating the map
 var activateMap = function () {
@@ -94,7 +109,6 @@ var setAddress = function (height) {
   var mainPinY = Math.round(mainPin.offsetTop + height);
   addressInput.value = mainPinX + ', ' + mainPinY;
 };
-setAddress(INITIAL_MAIN_PIN_HEIGHT / 2);
 
 // We are forming here an array of avatars and mix them
 var getAvatarsArray = function () {
@@ -168,6 +182,7 @@ var renderMapPin = function (mapPin) {
     }
   });
 
+  mapPins.push(mapPinElement);
   return mapPinElement;
 };
 
@@ -268,6 +283,9 @@ var openMapCard = function (mapCard) {
 // Close the ad, remove the event listener
 var closeMapCard = function () {
   var popup = document.querySelector('.map__card');
+  if (!popup) {
+    return;
+  }
   mapElement.removeChild(popup);
   document.removeEventListener('keydown', onMapCardEscPress);
 };
@@ -281,5 +299,98 @@ var mainPinMouseupHandler = function () {
   mainPin.removeEventListener('mouseup', mainPinMouseupHandler);
 };
 
-// Event listener activates the page
-mainPin.addEventListener('mouseup', mainPinMouseupHandler);
+// Function for activating the page
+var initialPage = function () {
+  disableForm();
+  setAddress(INITIAL_MAIN_PIN_HEIGHT / 2);
+  mainPin.addEventListener('mouseup', mainPinMouseupHandler);
+};
+
+initialPage();
+
+// Function for setting correlation between type of appartment and min price
+var setMinPrice = function (price) {
+  priceInput.min = price;
+  priceInput.placeholder = price;
+};
+
+typeField.addEventListener('change', function (evt) {
+  setMinPrice(minPrice[evt.target.value]);
+});
+
+// Setting the correlation between check-in and check-out
+checkInField.addEventListener('change', function (evt) {
+  checkOutField.value = evt.target.value;
+});
+
+checkOutField.addEventListener('change', function (evt) {
+  checkInField.value = evt.target.value;
+});
+
+// Disable the choice of the guests before the quantity of rooms, initial choice of guest number
+var setInitialCapacity = function () {
+  var capacityOption = capacityField.querySelectorAll('option');
+  capacityOption.forEach(function (item) {
+    item.setAttribute('disabled', 'disabled');
+
+    if (item.selected) {
+      item.removeAttribute('disabled');
+    }
+  });
+};
+setInitialCapacity();
+
+// Setting the correlation between quantity of rooms and guests
+var setCapacity = function (evt) {
+  if (+roomNumberField.value === MAX_ROOM_NUMBER) {
+    capacityField.value = NO_GUEST_VALUE;
+  } else {
+    capacityField.value = roomNumberField.value;
+  }
+
+  for (var i = 0; i < capacityField.length; i++) {
+    var option = capacityField.options[i];
+    var noGuests = +option.value === NO_GUEST_VALUE;
+    var tooManyGuests = +option.value > +evt.target.value;
+    if (+evt.target.value !== MAX_ROOM_NUMBER) {
+      option.disabled = noGuests || tooManyGuests;
+    } else {
+      option.disabled = !noGuests;
+    }
+  }
+};
+
+roomNumberField.addEventListener('change', setCapacity);
+
+// Setting the page to the initial
+resetButton.addEventListener('click', function () {
+  resetForm();
+  resetMap();
+  initialPage();
+});
+
+var resetForm = function () {
+  adForm.reset();
+  adForm.classList.add('ad-form--disabled');
+};
+
+var resetMap = function () {
+  closeMapCard();
+  mapElement.classList.add('map--faded');
+  mapPins.forEach(function (item) {
+    item.remove();
+  });
+};
+
+// Adding the red error frame to invalid elements and deleting it by corrections
+var inputs = adForm.querySelectorAll('input, select, textarea');
+inputs.forEach(function (input) {
+  input.addEventListener('invalid', function () {
+    input.classList.add('error');
+  });
+  input.addEventListener('input', function () {
+    if (input.validity.valid) {
+      input.classList.remove('error');
+    }
+  });
+});
